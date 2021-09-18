cbbefedc736d789eafd01b6bbbe974aae2943f36
SecondRod <85537227+SecondRod@users.noreply.github.com>
2021-09-18 11:25:30 -0700

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //first commit added
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+            if (nums == null || nums.Length < 2)
+                return new int[2];
+
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(target - nums[i]))
+                    return new int[] { i, dic[target - nums[i]] };
+                else if (!dic.ContainsKey(nums[i]))
+                    dic.Add(nums[i], i);
+            }
+
+            return new int[2];
+        }
+    }
+}
